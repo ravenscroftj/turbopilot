7eb10b225e740e5adeedac8ab85dd5992c5bda7a
James Ravenscroft <ravenscroftj@gmail.com>
2023-07-23 20:17:40 +0100

    remove weird server.hpp stuff

--- include/turbopilot/server.hpp
+++ include/turbopilot/server.hpp
@@ -8,11 +8,6 @@
 
 crow::response serve_response(TurbopilotModel *model, const crow::request& req);
 
-extern "C"
-crow::response server_response(const crow::request& req)
-{
-    return serve_response(&model, req);
-}
 
 
 #endif // __TURBOPILOT_SERVER_H
